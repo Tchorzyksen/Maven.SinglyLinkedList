4c646e2439e3fd8effc9307632fbba2eb730c195
Michal Tchorzewski <evife2@gmail.com>
2019-04-18 17:45:22 +0200

    Update .gitignore, new functionality push_front & display_list

--- src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
@@ -4,4 +4,16 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class MainApplication {
+    public static void main(String[] args) {
+
+        SinglyLinkedList myLinkedList=new SinglyLinkedList();
+
+        myLinkedList.add_node(97, "a");
+        myLinkedList.add_node(98, "b");
+        myLinkedList.add_node(99, "c");
+
+        myLinkedList.display_list();
+
+
+    }
 }
--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,86 @@
 package com.zipcodewilmington.singlylinkedlist;
 
-/**
- * Created by leon on 1/10/18.
- */
 public class SinglyLinkedList {
-}
+
+    private class Node { // Inner class node declaration.
+
+        private int id;
+
+        private String name;
+
+        private Node next_node; // Next element ref.
+
+        public Node(int id, String name) { // Constructor.
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Node getNext_node() {
+            return next_node;
+        }
+
+        public void setNext_node(Node next_node) {
+            this.next_node = next_node;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+
+    } // end Node declaration.
+
+    private Node head; // First element ref.
+
+    public SinglyLinkedList(Node head) { // Initialise list with already existing one.
+        this.head = head; // Shallow Copy
+    }
+
+    public SinglyLinkedList(){
+        this(null);
+    }
+
+    public boolean add_node(int id, String name){
+        // Push front.
+        Node new_node = new Node(id, name); // Create new element.
+
+        new_node.setNext_node(this.head); // Set next element to head.
+        this.head=new_node; // Set head to new element.
+
+        return this.head.getId()==id;
+    }
+
+    public void display_list(){
+
+        Node traverse =this.head; // Preserve head. Create copy of head to traverse list.
+
+        while(traverse!=null){
+
+            System.out.println("Id: " + traverse.getId()
+                    + "\tName: " + traverse.getName() + "\r");
+
+            traverse=traverse.next_node;
+        }
+
+    }
+
+} // end SinglyLinkedList declaration.
