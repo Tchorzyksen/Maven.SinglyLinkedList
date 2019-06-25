4c818847ab0c0a334e40e6a816aa2181125876e0
Michal Tchorzewski <evife2@gmail.com>
2019-06-25 14:58:54 +0200

    Generics, analysing build-in implementation of doubly linked list.

--- src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/MainApplication.java
@@ -1,29 +1,14 @@
 package com.zipcodewilmington.singlylinkedlist;
 
-/**
- * Created by leon on 1/10/18.
- */
 public class MainApplication {
     public static void main(String[] args) {
 
-        SinglyLinkedList myLinkedList=new SinglyLinkedList();
+        SinglyLinkedList<Integer> myLinkedList = new SinglyLinkedList<>();
 
-        myLinkedList.push_front(97, "a");
-        myLinkedList.push_front(98, "b");
-        myLinkedList.push_front(99, "c");
-        myLinkedList.push_back(100, "d");
-        myLinkedList.push_back(101, "e");
-        myLinkedList.push_back(102, "f");
+        myLinkedList.addFirst(Integer.valueOf(10));
+        myLinkedList.addFirst(Integer.valueOf(12));
 
-        System.out.println("Element with id 69\tis at index:\t" + myLinkedList.find(69));
-        System.out.println("Element with id 97\tis at index:\t" + myLinkedList.find(97));
-        System.out.println("Element with id 99\tis at index:\t" + myLinkedList.find(99));
-        System.out.println("Element with id 102\tis at index:\t" + myLinkedList.find(102));
-        System.out.println("Remove first element " + myLinkedList.removeFirst());
-
-        myLinkedList.display_list();
-        System.out.println("Number of elements inside list is: " + myLinkedList.size());
-
+        System.out.println(".get(0): " + myLinkedList.getFirst());
 
 
     }
--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -2,166 +2,70 @@ package com.zipcodewilmington.singlylinkedlist;
 
 import java.util.NoSuchElementException;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<E> {
 
-    private class Node { // Inner class node declaration.
+    Node<E> first; // Reference to the first Node.
 
-        private int id;
+    private int size = 0;
 
-        private String name;
+    public SinglyLinkedList(){
 
-        private Node next_node; // Next element ref.
-
-        public Node(int id, String name) { // Constructor.
-            this.id = id;
-            this.name = name;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public Node getNext_node() {
-            return next_node;
-        }
-
-        public void setNext_node(Node next_node) {
-            this.next_node = next_node;
-        }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    '}';
-        }
-
-    } // end Node declaration.
-
-    private Node head; // First element ref.
-
-    private Node tail; // Last element ref.
-
-    private int element_count; // Store number of elements.
-
-    private SinglyLinkedList(Node head, Node tail, int element_count) {
-        this.head = head;
-        this.tail = tail;
-        this.element_count = element_count;
     }
 
-    public SinglyLinkedList(SinglyLinkedList singlyLinkedList) { // Initialise list with already existing one.
-        this.head = singlyLinkedList.getHead(); // Shallow Copy
-        this.tail = singlyLinkedList.getTail();
-        this.element_count = singlyLinkedList.size();
+    /**
+     * Links e as first element.
+     */
+    private void linkFirst(E e){
+        final Node<E> f = first;
+        final Node<E> newNode = new Node<>(e, f);
+
+        first = newNode;
+        size++;
     }
 
-    public SinglyLinkedList(){ // Shallow copy...
-        this(null, null, 0);
+    /**
+     * Inserts the specified element at the beginning of this list.
+     *
+     * @param e the element to add
+     */
+    public void addFirst(E e){
+        linkFirst(e);
     }
 
-    private Node getHead() {
-        if (this.head == null)
+    /**
+     * Returns the first element in this list.
+     *
+     * @return the first element in this list
+     * @throws NoSuchElementException if this list is empty
+     */
+    public E getFirst(){
+        final Node<E> f = first;
+        if(f == null)
             throw new NoSuchElementException();
 
-        return this.head;
+        return f.item;
     }
 
-    private Node getTail() {
-        if (this.head == null)
-                throw new NoSuchElementException();
-
-        return this.tail;
+    /**
+     * Returns the number of the elements in the list.
+     *
+     * @return the number of elements in the list.
+     */
+    public int getSize(){
+        return size;
     }
 
-    public int size() {
-        return element_count;
-    }
+    // Static nested class
+    // Do not have access to other members of the enclosing class
+    private static class Node<E> {
+        E item;
+        Node<E> next;
 
-    public void push_front(int id, String name){
-        // Push front.
-        Node new_node = new Node(id, name); // Create new element.
-
-        if(this.head==null) // Only one element inside list results in head and tail to point at the same node.
-            this.tail=new_node;
-        else
-            new_node.setNext_node(this.head); // Set next element to head.
-
-        this.head=new_node; // Set head to new element.
-
-        element_count++;
-    }
-
-    public void push_back(int id, String name){
-
-        Node new_node = new Node(id, name);
-
-        if(this.head==null) // list empty add new element as head.
-            this.head = new_node;
-        else
-            this.tail.setNext_node(new_node);
-
-        this.tail = new_node;
-
-        element_count++;
-    }
-
-    public int find(int id){
-
-        int index=0;
-        for(Node traverse = this.head; traverse!=null; traverse=traverse.getNext_node()){
-
-            if(traverse.getId()==id)
-                return index;
-
-            index++;
+        Node(E element, Node<E> next){
+            this.item = element;
+            this.next = next;
         }
 
-        return -1; // Failed to find element with given index.
-    }
-
-    public Node removeFirst(){
-
-        if (this.head == null)
-            throw new NoSuchElementException();
-
-        Node f = this.head;
-
-        if (element_count == 1) {
-            this.tail = null;
-        }
-        this.head = this.head.next_node;
-        element_count--;
-
-        return f;
-    }
-
-    public void display_list(){
-
-        Node traverse =this.head; // Preserve head. Create copy of head to traverse list.
-
-        int index=0;
-        while(traverse!=null){
-
-            System.out.println("Index: " + (index++) + "\t\tId: " + traverse.getId()
-                    + "\t\tName: " + traverse.getName() + "\r");
-
-            traverse=traverse.next_node;
-        }
-
-    }
+    } // end generic Node declaration.
 
 } // end SinglyLinkedList declaration.
